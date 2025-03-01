1e101995c3ab9d80c350ca8bee1ed362acb022d1
kiwy36 <kevinwittner92@gmail.com>
2025-02-28 22:27:19 -0300

    add bucles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './components/Home'
 import FuncionesDemo from './components/FuncionesDemo'
+import ClassTres from './components/ClassTres'
 function App() {
 
   return (
     <>
       <Home/>
       <FuncionesDemo/>
+      <ClassTres/>
     </>
   )
 }
--- src/components/ClassTres.tsx
+++ src/components/ClassTres.tsx
@@ -1,4 +1,23 @@
 const ClassTres = () => {
+  const arrayUno = ['elementoUno', 'elementoDos']
+  console.log (arrayUno[0])
+  console.log(arrayUno.length)
+  arrayUno[0]='elemento sobreescribido'
+  console.log (arrayUno[0])
+  arrayUno.push('elementoTres')
+  console.log (arrayUno[2])
+  let num  = 0;
+  while (num <20){
+    console.log(num)
+    num = num +1
+
+  }
+  let i
+  for (i =0 ; i<= 5; i++){
+    console.log('coso')
+  }
+
+
   return (
     <div>
       dia de clases
